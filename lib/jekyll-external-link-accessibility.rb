96884dbeef36290d7b44f17f4e1c416fc54f56ae
Kudakwashe Siziva <kudakwashe@ombulabs.com>
2023-04-05 20:55:03 +0200

    Add configurations for excluded links

--- lib/jekyll-external-link-accessibility.rb
+++ lib/jekyll-external-link-accessibility.rb
@@ -1,42 +1,56 @@
+require 'jekyll-external-link-accessibility/hooks'
 require 'nokogiri'
 
 module Jekyll
-  module ExternalLinkAccessibility
-    class Generator < Jekyll::Generator
-      def generate(site)
-        site.posts.docs.each { |post| process_post(post) }
-      end
+  class ExternalLinkAccessibility
+    def self.modify_links(page)
+      config = page.site.config
+      doc = Nokogiri::HTML.fragment(page.output)
+      doc.css('a').each do |a|
+        next if a['href'].nil? || a['href'].empty? || a['href'] == '#' || excludes_external_link?(config: config, url: a['href'])
+        next if excludes_external_link?(config: config, url: a['href'])
 
-      private
-
-      def process_post(post)
-        doc = Nokogiri::HTML.fragment(post.content)
-        modify_links(doc)
-        post.content = doc.to_html
-      end
-
-      def modify_links(doc)
-        doc.css('a').each do |a|
-          next if a['href'].nil? || a['href'].empty? || a['href'] == '#' || a['target'].nil?
-          if a['href'].start_with?('http')
-            a['rel'] = 'noreferrer noopener'
-            a['data-type'] = 'URL'
-            a['title'] = 'Opens a new window'
-            a.add_child(" <i class='icon-external-link' aria-hidden='true'></i>")
-            a.add_child(
-              "<span 
-                style='overflow: hidden;clip: rect(0,0,0,0);
-                      position: absolute !important;
-                      width: 1px;
-                      height: 1px;
-                      border: 0;
-                      word-wrap: normal !important;'>
-                opens a new window
-              </span>"
-            )
-          end
+        if a['href'].start_with?('http')
+          a['rel'] = external_link_rel(config: config) unless a['rel']
+          a['target'] = external_link_target(config: config) unless a['target']
+          a['title'] = external_link_title(config: config) unless a['title']
+          a.add_child(" <i class='icon-external-link' aria-hidden='true'></i>")
+          a.add_child(
+            "<span
+              style='overflow: hidden;clip: rect(0,0,0,0);
+                    position: absolute !important;
+                    width: 1px;
+                    height: 1px;
+                    border: 0;
+                    word-wrap: normal !important;'>
+              opens a new window
+            </span>"
+          )
         end
       end
+      page.output = doc.to_html
     end
+
+    def self.excludes_external_link?(config:, url:)
+      excludes = (config.dig('external_links', 'exclude') || [])
+      excludes.any? { |exclude| Regexp.new("^#{exclude}$").match? url }
+    end
+
+    def self.external_link_rel(config:)
+      config.dig('external_links', 'rel') || 'external nofollow noopener noreferrer'
+    end
+
+    def self.external_link_target(config:)
+      config.dig('external_links', 'target') || '_blank'
+    end
+
+    def self.external_link_title(config:)
+      config.dig('external_links', 'title') || 'Opens a new window'
+    end
+
+    private_class_method :excludes_external_link?
+    private_class_method :external_link_rel
+    private_class_method :external_link_target
+    private_class_method :external_link_title
   end
 end
---
+++ lib/jekyll-external-link-accessibility/hooks.rb
@@ -0,0 +1,6 @@
+require 'jekyll/hooks'
+require 'jekyll-external-link-accessibility'
+
+Jekyll::Hooks.register :posts, :post_render do |page|
+  Jekyll::ExternalLinkAccessibility.modify_links(page)
+end
