d41b9c1ea77d8e5564cba423c59cda695ea771c3
Henrique Medeiros <hmdros@gmail.com>
2025-08-28 17:03:57 -0300

    OMBU-590 add post-excerpt class links to accessibility code

--- lib/jekyll-external-link-accessibility/hooks.rb
+++ lib/jekyll-external-link-accessibility/hooks.rb
@@ -4,3 +4,7 @@ require 'jekyll-external-link-accessibility'
 Jekyll::Hooks.register :posts, :post_render do |page|
   Jekyll::ExternalLinkAccessibility.modify_links(page)
 end
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+  Jekyll::ExternalLinkAccessibility.modify_links(page)
+end
